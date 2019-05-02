bde9c7bd164ccbc6d7c9cfc524f66f8b4244d082
Kogun Andrii <kogun.a@ualeaders.com>
2019-05-02 23:00:28 +0300

    .gitignore is now working

--- Pods/Target Support Files/Pods-CityDozor/Pods-CityDozor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CityDozor : NSObject
-@end
-@implementation PodsDummy_Pods_CityDozor
-@end
--- Pods/Target Support Files/Pods-CityDozorTests/Pods-CityDozorTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CityDozorTests : NSObject
-@end
-@implementation PodsDummy_Pods_CityDozorTests
-@end
